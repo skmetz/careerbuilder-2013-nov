87e64deaed65f65e9d3aa4d989d1182e932ee81e
Katrina Owen <katrina.owen@gmail.com>
2013-11-19 13:19:34 -0500

    Solve bottles in the simplest possible way

---
+++ lib/bottles.rb
@@ -0,0 +1,22 @@
+class BottlesSong
+  def sing
+    verses(99, 0)
+  end
+
+  def verses(starting, ending)
+    starting.downto(ending).map {|n| verse(n) }.join("\n") + "\n"
+  end
+
+  def verse(number)
+    case number
+    when 0
+      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    when 1
+      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+    when 2
+      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+    else
+      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
+    end
+  end
+end
