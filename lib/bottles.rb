9ff4e69cdefaf8a7b0607b3fe7b3f4aa91de6197
tkling <tyling@gmail.com>
2013-11-20 11:29:39 -0500

    yay syncing

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,58 +1,21 @@
+require './lib/beer_verse'
+
 class BottlesSong
+
+  def initialize(verse_class = BottlesOfBeerVerse)
+    @verse_class = verse_class
+  end
+
   def sing
     verses(99, 0)
   end
 
   def verses(starting, ending = 0)
-    (ending..starting).to_a.reverse.map {|n| verse(n) }.join("\n") + "\n"
+    starting.downto(ending).map { |n| verse(n) }.join("\n") + "\n"
   end
 
   def verse(number)
-    "#{count(number)} #{containers(number)} of beer on the wall, #{count(number).downcase} #{containers(number)} of beer.\n" +
-    action(number) +
-    "#{count(number - 1).to_s.downcase} #{containers(number - 1)} of beer on the wall.\n"
+    @verse_class.new(number).to_s
   end
 
-  private
-
-  def count(number)
-    case number
-    when -1
-      "99"
-    when 0
-      "No more"
-    when 1
-      "1"
-    else
-      number.to_s
-    end
-  end
-
-  def containers(number)
-    case number
-    when 1
-      "bottle"
-    else
-      "bottles"
-    end
-  end
-
-  def action(number)
-    case number
-    when 0
-      "Go to the store and buy some more, "
-    else
-      "Take #{pronoun(number)} down and pass it around, "
-    end
-  end
-
-  def pronoun(number)
-    case number
-    when 1
-      "it"
-    else
-      "one"
-    end
-  end
 end
-
